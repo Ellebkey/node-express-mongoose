76bc340ab8fc26e2b8d032c92f8ad3174fe532d6
Ellebkey <ellebkey@gmail.com>
2019-01-03 08:53:20 -0600

    F: Lint errors on auth helper.

--- server/helpers/auth.js
+++ server/helpers/auth.js
@@ -5,11 +5,11 @@ const config = require('../../config/config');
  * Middleware to validate user token
  */
 module.exports = (req, res, next) => {
-  if( typeof req.headers.authorization !== "undefined" ) {
+  if (typeof req.headers.authorization !== 'undefined') {
     try {
       req.user = jwt.verify(req.headers.authorization, config.jwtSecret);
       return next();
-    } catch(err) {
+    } catch (err) {
       return res.status(401).json({
         error: {
           message: 'Failed to authenticate token!',
--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -2,29 +2,25 @@ const Joi = require('joi');
 const _ = require('lodash');
 
 /** Validation function */
-module.exports = (joiObject) => {
-  return (req, res, next) => {
-    const data = { body: req.body, query: req.query, params: req.params}
+module.exports = joiObject => (req, res, next) => {
+  const data = { body: req.body, query: req.query, params: req.params };
 
-    const validate = Joi.validate(data, joiObject, { abortEarly: false, allowUnknown: true });
+  const validate = Joi.validate(data, joiObject, { abortEarly: false, allowUnknown: true });
 
-    if (validate.error) {
+  if (validate.error) {
+    const errorMessage = _
+      .chain(validate.error.details)
+      .map((o, idx) => `${idx + 1}. ${o.message}`)
+      .value();
 
-      const errorMessage = _
-        .chain(validate.error.details)
-        .map((o, idx) => `${idx + 1}. ${o.message}`)
-        .value();
+    const message = _.join(errorMessage, ', ').replace(/['"]+/g, '');
 
-      const message = _.join(errorMessage, ', ').replace(/['"]+/g, '');
-
-      return res.status(422)
-        .json({
-          message,
-          code: 422,
-          error: validate.error
-        });
-    }
-    return next();
+    return res.status(422)
+      .json({
+        message,
+        code: 422,
+        error: validate.error
+      });
   }
-
+  return next();
 };
